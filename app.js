9549f92db2ef459fda0efae01065978b732aec32
noorkhadija557 <noorkhadija557@gmail.com>
2026-09-12 20:36:24 +0500

    push

--- app.js
+++ app.js
@@ -14,3 +14,78 @@ const { createClient } = supabase;
 const client = createClient(supabaseUrl, supabaseKey)
 
 console.log(client);
+
+const signUpForm = document.querySelector("#signupForm")
+
+signUpForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    try {
+        const formData = new FormData(signUpForm)
+
+
+        let emptyField = false;
+        const inputs = document.querySelectorAll("input")
+        inputs.forEach((input) => {
+            if (input.value === "") {
+                input.style.border = "2px solid red"
+                emptyField = true;
+            }
+        })
+
+        if (emptyField) {
+            return
+        }
+
+        const data = Object.fromEntries(formData)
+
+        const { name, email, password, repeatpassword } = data
+
+
+        const { data: signUpData, error } = await client.auth.signUp({
+            email,
+            password,
+        });
+
+        if (error) {
+    console.log("Signup Error:", error.message);
+    alert(error.message);
+    return;
+}
+
+console.log("Signup successful:", signUpData);
+
+
+        const id = signUpData?.user?.id
+        console.log(id);
+
+        // database insertion
+        const { error: databaseError } = await client
+            .from('blog_data')
+            .insert({
+                name,
+
+            })
+        console.log(databaseError)
+
+        if (signUpData) {
+            console.log(signUpData);
+        }
+        else {
+            console.log(error.message)
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+})
+
+const inputs = document.querySelectorAll("input")
+inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+        if (input.value !== "") {
+            input.style.border = ""
+
+        }
+    })
+
+})
