92da3e4a61f15a3b19421683bd08958540a9d0f2
noorkhadija557 <noorkhadija557@gmail.com>
2026-09-13 14:08:53 +0500

    push

--- login.js
+++ login.js
@@ -1,3 +1,11 @@
+// Navbar
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("#navbar").innerHTML = data;
+    });
+
+
 const supabaseUrl = "https://xmexfecjjalkhqtrlzzj.supabase.co";
 const supabaseKey = "sb_publishable_MscDQGxX8gej_btcdCaQjA_6qODt-W8";
 
